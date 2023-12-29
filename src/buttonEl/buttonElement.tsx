573b551d67c506ce64f56edf3219e76dfc5ec4e6
dawidj <dawidjanicki2@gmail.com>
2023-12-29 18:16:56 +0100

    Add component buttonElement

---
+++ src/buttonEl/buttonElement.tsx
@@ -0,0 +1,16 @@
+/*
+function Button() {
+  return <button>Przejdz dalejjjjj </button>;
+}
+*/
+export const Button = ({
+  buttonName,
+  url,
+}: {
+  buttonName: string;
+  url: string;
+}) => {
+  return <button>PRZEJDZ DALEJJJ</button>;
+};
+
+export default Button;
